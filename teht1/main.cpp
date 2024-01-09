8fe4db298cb4ab1b16354e60bfba6f6ebc464660
ArttuIivari <89580030+ArttuIivari@users.noreply.github.com>
2024-01-09 11:08:56 +0200

    fdasdfads

---
+++ teht1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "aika siisti tehtävä tää näin öö joo eikö vaan tosiaan aika paljon löytyy kaikkee siistii juttuu täältä koulusta tuollaki on tuommonen taulu jossa on paljon tekstejä ja tuossa on oskukyklooppeja jokka on kyllä tosi siistejä ja sitte tossa on jotaki servereitä tai jotain emt näyttää ainaki sellasilta" << endl;
+    return 0;
+}
