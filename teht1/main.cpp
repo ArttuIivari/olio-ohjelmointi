33fba3a883907da89ae6c8e5d1e9394666e499e5
ArttuIivari <89580030+ArttuIivari@users.noreply.github.com>
2024-01-10 13:42:22 +0200

    kotitehtävä 1 vihdoin valmis??

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -3,17 +3,25 @@
 #include <ctime>
 
 using namespace std;
-
+int game(int);
 int main()
 {
+
+    int arvausmaara = game(40);
+    cout << "Arvausten maara: " << arvausmaara << endl;
+
+    return 0;
+}
+int game(int maxnum){
     srand(time(NULL));
-    int satunnaisluku = rand() % 20;
+    int satunnaisluku = rand() % maxnum;
     if(satunnaisluku==0){
-        satunnaisluku = 20;
+        satunnaisluku = maxnum;
     }
     int arvaus=99;
+    int arvauscount=0;
     while(arvaus!=satunnaisluku){
-        cout << "Arvaa numero 1-20 valilta" << endl;
+        cout << "Arvaa numero 1-40 valilta" << endl;
         cin >> arvaus;
         if(arvaus>satunnaisluku){
             cout << "Luku on pienempi." << endl;
@@ -24,7 +32,11 @@ int main()
         else if(arvaus==satunnaisluku){
             cout << "Oikea vastaus!" << endl;
         }
+        arvauscount++;
     }
-
-    return 0;
+    return arvauscount;
 }
+
+
+
+
