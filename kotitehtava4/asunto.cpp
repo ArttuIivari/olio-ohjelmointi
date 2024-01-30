f257aff5dbc82b2ff3a51cb5e57cee528e84eb14
ArttuIivari <89580030+ArttuIivari@users.noreply.github.com>
2024-01-30 12:59:25 +0200

    Kotitehtävä 3 Ensimmäinen dia

--- kotitehtava4/asunto.cpp
+++ kotitehtava4/asunto.cpp
@@ -2,5 +2,24 @@
 
 Asunto::Asunto()
 {
+    cout << "Konstruktori luotu" << endl;
+}
 
+Asunto::~Asunto()
+{
+    cout << "destruktori luotu" << endl;
+}
+
+void Asunto::maarita(int lkm, int nelio)
+{
+    cout << "Asunto maaritetty, asukkaita = " << lkm << " nelioita = " << nelio << endl;
+    asukasMaara = lkm;
+    neliot = nelio;
+}
+
+double Asunto::laskeKulutus(double price)
+{
+    int vastaus = asukasMaara * neliot;
+    cout << "Asunnon kulutus, kun hinta = " << price << " on " << vastaus << endl;
+    return vastaus;
 }
--- kotitehtava4/main.cpp
+++ kotitehtava4/main.cpp
@@ -1,9 +1,17 @@
+#include "asunto.h"
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    Asunto objectAsunto;
+    objectAsunto.maarita(2,100);
+    double f = objectAsunto.laskeKulutus(1);    //Täytyy returnata johonkin muuttujaan,
+                                                //muuten ei koskaan luo destruktoria
+
+
     return 0;
 }
