e5a50d54748b142c17baeb17de17501ca8fb3eba
ArttuIivari <89580030+ArttuIivari@users.noreply.github.com>
2024-02-20 23:17:01 +0200

    Kotitehtävä 6 valmis

--- kotitehtava6/mainwindow.cpp
+++ kotitehtava6/mainwindow.cpp
@@ -13,60 +13,85 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-/*
-void MainWindow::on_btn_count_clicked()
-{
-    p = ui->textNumber->text();
-
-    value = p.toInt();
-    value++;
-    ui->textNumber->setText(QString::number(value));
-}
-
-
-void MainWindow::on_btn_reset_clicked()
-{
-    ui->textNumber->setText("0");
-}
-*/
 void MainWindow::numberClickedHandler()
 {
     QPushButton *button = qobject_cast<QPushButton*>(sender());
     int digitvalue = button->text().toInt();
     QString name = QString::number(digitvalue);
-    qDebug() << "Button name: " << digitvalue;
-    /*
-    QPushButton *button = qobject_cast<QPushButton*>(sender());
-    QString name = button->objectName();
-    qDebug() << "button name: " << name;
-    number1 = number1 + name.last(1);
-    */
-
-    if(state==0){
-        qDebug() << " fdfdsfadsf";
-        QString text = ui->lineEditNumber1->text();
-        ui->lineEditNumber1->setText(text + name);
+    qDebug() << "Button name: " << name;
+    switch(state){
+    case 0:
+        ui->lineEditNumber1->setText(number1 + name);
+        number1 = ui->lineEditNumber1->text();
+        break;
+    case 1:
+        ui->lineEditNumber2->setText(number2 + name);
+        number2 = ui->lineEditNumber2->text();
+        break;
     }
 
-
 }
-
 void MainWindow::clearAndEnterClickHandler()
 {
     QPushButton *button = qobject_cast<QPushButton*>(sender());
     QString name = button->objectName();
     qDebug() << "button name " << name;
     if(name=="btnClear"){
-
-        ui->lineEditNumber1->setText("");
-        ui->lineEditNumber2->setText("");
-        number1=0;
-        number2=0;
+        resetLineEdits();
     }
-
+    if(name=="btnEnter"){
+        float n1 = number1.toFloat();
+        float n2 = number2.toFloat();
+        qDebug() << "num1 " << n1 << "num2 " << n2;
+        qDebug() << operand;
+        switch(operand){
+        case 0:
+            result=n1+n2;
+            break;
+        case 1:
+            result=n1-n2;
+            break;
+        case 2:
+            result=n1*n2;
+            break;
+        case 3:
+            result=n1/n2;
+            break;
+        default:
+            qDebug() << "Dafuq";
+            return;
+        }
+        ui->lineEditResult->setText(QString::number(result));
+    }
 }
 
+void MainWindow::addSubMulDivClickHandler()
+{
+    QPushButton *button = qobject_cast<QPushButton*>(sender());
+    QString name = button->objectName();
+    qDebug() << "button name: " << name;
+    if(name=="btnPlus")
+        operand = 0;
+    else if(name=="btnMinus")
+        operand = 1;
+    else if(name=="btnMultiply")
+        operand = 2;
+    else if(name=="btnDiv")
+        operand = 3;
+    else operand = 99;
+    state = 1;
+}
 
+void MainWindow::resetLineEdits()
+{
+    ui->lineEditNumber1->setText("");
+    ui->lineEditNumber2->setText("");
+    ui->lineEditResult->setText("");
+    number1=0;
+    number2=0;
+    state=0;
+    operand=99;
+}
 
 void MainWindow::on_btn1_clicked()
 {
@@ -137,5 +162,28 @@ void MainWindow::on_btnClear_clicked()
 void MainWindow::on_btnEnter_clicked()
 {
     clearAndEnterClickHandler();
+}
+
+
+void MainWindow::on_btnPlus_clicked()
+{
+    addSubMulDivClickHandler();
+}
+
+
+void MainWindow::on_btnMinus_clicked()
+{
+    addSubMulDivClickHandler();
+}
+
+
+void MainWindow::on_btnMultiply_clicked()
+{
+    addSubMulDivClickHandler();
+}
+
+void MainWindow::on_btnDiv_clicked()
+{
+    addSubMulDivClickHandler();
 }
 
--- kotitehtava6/mainwindow.h
+++ kotitehtava6/mainwindow.h
@@ -16,40 +16,28 @@ public:
     ~MainWindow();
 
 private slots:
-
-
-
-
     void on_btn1_clicked();
-
     void on_btn2_clicked();
-
     void on_btn3_clicked();
-
     void on_btn4_clicked();
-
     void on_btn5_clicked();
-
     void on_btn6_clicked();
-
     void on_btn7_clicked();
-
     void on_btn8_clicked();
-
     void on_btn9_clicked();
-
     void on_btn0_clicked();
-
     void on_btnClear_clicked();
-
     void on_btnEnter_clicked();
-
+    void on_btnPlus_clicked();
+    void on_btnMinus_clicked();
+    void on_btnMultiply_clicked();
+    void on_btnDiv_clicked();
 private:
     Ui::MainWindow *ui;
     QString number1, number2;
     int state = 0;
     float result;
-    short operand;
+    short operand=4;
 
     void numberClickedHandler();
     void clearAndEnterClickHandler();
